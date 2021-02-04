6611ece7d66459ff106ed47f61eac9ff5bc9a10b
Slava314 <vorobev.stu@gmail.com>
2021-02-05 00:23:05 +0300

    add deck_shape

--- source/main.cpp
+++ source/main.cpp
@@ -2,7 +2,7 @@
 #include "button.h"
 
 int main() {
-    sf::RenderWindow start_window(sf::VideoMode(1000, 800), "Start_window", sf::Style::Default);
+    sf::RenderWindow start_window(sf::VideoMode(1500, 1000), "Start_window", sf::Style::Default);
 
     sf::Font font;
     font.loadFromFile("../times.ttf");
@@ -14,20 +14,27 @@ int main() {
                          (start_window.getSize().y -
                           start_button.get_shape().getSize().y) / 2.0));
 
-    //start_button.set_text_size(20);
 
+    bool new_window = 0;
     while (start_window.isOpen()) {
         sf::Event event;
         while (start_window.pollEvent(event)) {
-            if (event.type == sf::Event::Closed)
+            if (event.type == sf::Event::Closed) {
                 start_window.close();
+            }
+
             if (event.type == sf::Event::MouseButtonPressed) {
                 if (event.mouseButton.button == sf::Mouse::Left) {
-                    if(start_button.is_clicked(sf::Mouse::getPosition(start_window))){
+                    if (start_button.is_clicked(sf::Mouse::getPosition(start_window))) {
+                        new_window = 1;
                         start_window.close();
                     }
                 }
             }
+
+//            if (event.type == sf::Event::Resized) {
+//                start_button.set_text_size(32);
+//            }
         }
 
 
@@ -35,6 +42,42 @@ int main() {
         start_window.draw(start_button.get_shape());
         start_window.draw(start_button.get_text());
         start_window.display();
+    }
+
+    if (new_window) {
+
+        //создание игры с колодой игроками и тд наверное в другой файл вообще всё надо вынести
+
+        sf::RenderWindow game_window(sf::VideoMode(1500, 1000), "Game_window", sf::Style::Default);
+
+        sf::RectangleShape deck_shape({140, 210});
+        deck_shape.setFillColor(sf::Color(73, 215, 115));
+        deck_shape.setOutlineThickness(5);
+        deck_shape.setOutlineColor(sf::Color(4, 137, 44));
+
+        deck_shape.setPosition((game_window.getSize().x - deck_shape.getSize().x) / 2.0f,
+                               (game_window.getSize().y / 3.0f - deck_shape.getSize().y) / 2.0f);
+
+        sf::Text deck_text("deck: 0", font); //need number of cards
+
+        deck_text.setPosition(deck_shape.getPosition().x + (deck_shape.getSize().x - deck_text.getGlobalBounds().width) / 2.0f,
+                              deck_shape.getPosition().y);
+
+
+
+        while (game_window.isOpen()) {
+            sf::Event event;
+            while (game_window.pollEvent(event)) {
+                if (event.type == sf::Event::Closed)
+                    game_window.close();
+            }
+
+
+            game_window.clear();
+            game_window.draw(deck_shape);
+            game_window.draw(deck_text);
+            game_window.display();
+        }
     }
 
     return 0;
