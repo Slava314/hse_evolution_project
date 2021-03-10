0dde314394d5b7c3e01afbd266808fed2caef504
Slava314 <vorobev.stu@gmail.com>
2021-03-10 23:25:25 +0300

    new file

---
+++ include/window_fwd.h
@@ -0,0 +1,7 @@
+#ifndef EVOLUTION_PROJECT_INCLUDE_WINDOW_FWD_H_
+#define EVOLUTION_PROJECT_INCLUDE_WINDOW_FWD_H_
+
+class Window;
+class StartWindow;
+class GameWindow;
+#endif  // EVOLUTION_PROJECT_INCLUDE_WINDOW_FWD_H_
